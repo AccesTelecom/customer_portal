3ff61fd29b2304791f2e6cc5e6ba94bfb1a9ff58
mcampagna1 <mcampagna@accestelecom.ca>
2017-06-08 12:59:50 -0400

    Create general.php

--- portal/resources/lang/fr/general.php
+++ portal/resources/lang/fr/general.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'date' => 'Date',
-    'amount' => 'Amount',
-    'accountNumber' => 'Account Number',
-    'notAvailable' => 'Not Available',
-    'yes' => 'Yes',
-    'no' => 'No',
+    'amount' => 'Montant',
+    'accountNumber' => 'Numéro de compte',
+    'notAvailable' => 'Non disponible',
+    'yes' => 'Oui',
+    'no' => 'Non',
 ];
