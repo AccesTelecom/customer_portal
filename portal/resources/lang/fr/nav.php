967f4776a280e93c40453ee43bef31b6c1a218c9
mcampagna1 <mcampagna@accestelecom.ca>
2017-06-08 13:17:12 -0400

    Create nav.php

--- portal/resources/lang/fr/nav.php
+++ portal/resources/lang/fr/nav.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'logOut' => 'Log Out',
-    'billing' => 'Billing',
-    'profile' => 'My Details',
-    'tickets' => 'Support Tickets',
-    'dataUsage' => 'Data Usage',
-    'contracts' => 'Contracts',
+    'logOut' => 'Se déconnecter',
+    'billing' => 'Facturation',
+    'profile' => 'Mes détails',
+    'tickets' => 'Soutien pour billet',
+    'dataUsage' => 'Utilisation de données',
+    'contracts' => 'Contrats',
 ];
